838cb5f4691bf372d2b587d797bc88fa4a88031f
Phil Nash <philnash@gmail.com>
2023-08-31 21:27:35 +1000

    Handle error if coverage directory isn't present

--- test/remove_coverage.ts
+++ test/remove_coverage.ts
@@ -1,1 +1,5 @@
-Deno.removeSync("coverage", { recursive: true });
+try {
+  Deno.removeSync("coverage", { recursive: true });
+} catch (_error) {
+  // There was no coverage directory, nothing to do.
+}
